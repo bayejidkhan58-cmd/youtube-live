5d039b8509abdfe202bbe31ce673c33b83db0ab0
bayejidkhan58-cmd <bayejidkhan58@gmail.com>
2026-06-11 14:45:13 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VIDEO_URL="https://www.dropbox.com/scl/fi/fw544gii8ww6co101vli9/.mp4.mp4?rlkey=u7b6tfmbgn3k8lq9wkqrjvsyf&st=fp42xyts&dl=1"
+VIDEO_URL="https://storage.to/xs8Ajr7tM"
 STREAM_KEY=pdu9-wert-z2se-zw9w-dg7r
 ffmpeg -re -stream_loop -1 -i "$VIDEO_URL" -c:v libx264 -preset ultrafast -b:v 1000k -c:a aac -b:a 96k -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
