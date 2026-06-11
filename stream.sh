9afbf1afa3fd94e457b079b41ad63a55bd857993
bayejidkhan58-cmd <bayejidkhan58@gmail.com>
2026-06-11 15:13:45 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-VIDEO_URL="https://storage.to/xs8Ajr7tM"
 STREAM_KEY=pdu9-wert-z2se-zw9w-dg7r
-ffmpeg -re -stream_loop -1 -i "$VIDEO_URL" -c:v libx264 -preset ultrafast -b:v 1000k -c:a aac -b:a 96k -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
+ffmpeg -re -stream_loop -1 -i "video.mp4" -c:v libx264 -preset ultrafast -b:v 1500k -c:a aac -b:a 128k -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
